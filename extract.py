9352a1ac439c19fe95d8c9522c091c51e69b206f
alxdembo <alex.dembo@sainsburys.co.uk>
2021-01-20 22:11:03 +0000

    specify columns test passes

--- extract.py
+++ extract.py
@@ -4,7 +4,15 @@ import pandas as pd
 class CargoData:
     def __init__(self, source_file):
         self.source_file = source_file
-        self.source_df = pd.read_parquet(source_file)
+        self.source_df = pd.read_parquet(source_file, engine='pyarrow', columns=[
+            "start_timestamp",
+            "end_timestamp",
+            "vessel_class",
+            "id"
+        ])
+
+    def get_duration(self):
+        return self.source_df.end_timestamp - self.source_df.start_timestamp
 
     def print_movements(source_file):
         raise NotImplementedError
@@ -12,6 +20,7 @@ class CargoData:
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("source", help="A source file to extract movements from.")
     args = parser.parse_args()
--- test_extract.py
+++ test_extract.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import pytest
+from extract import CargoData
+from datetime import datetime, timedelta
 
 @pytest.fixture()
 def cargo_movements(tmpdir):
     df = pd.DataFrame({
-        "start_timestamp": ['2000-01-01T00:00:00+0000', '2000-01-01T00:00:00+0000', '2000-01-01T00:00:00+0000'],
-        "end_timestamp": ['2000-01-02T00:02:00+0000', '2000-01-02T00:02:00+0000', '2000-01-02T00:02:00+0000'],
+        "start_timestamp": [datetime(2000, 1, 1, 1, 1, 1), datetime(2000, 1, 1, 1, 1, 1), datetime(2000, 1, 1, 1, 1, 1)],
+        "end_timestamp": [datetime(2000, 1, 1, 1, 1, 2), datetime(2000, 1, 1, 1, 1, 3), datetime(2000, 1, 1, 1, 1, 4)],
         "vessel_class": ["test_class_1", "test_class_1", "test_class_2"],
         "id": ["test_id_1", "test_id_2", "test_id_3"]
     })
@@ -17,6 +19,11 @@ def cargo_movements(tmpdir):
 
 
 def test_print_movements(cargo_movements):
-    pd.read_parquet(cargo_movements)
-
-    assert False
+    cc = CargoData(cargo_movements)
+    actual = cc.get_duration().to_dict()
+    expected = {
+        0: timedelta(0,1),
+        1: timedelta(0,2),
+        2: timedelta(0,3)
+    }
+    assert actual == expected
