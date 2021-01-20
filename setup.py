535ae8c84587685046c9177f4b966c9208baad61
alxdembo <alex.dembo@sainsburys.co.uk>
2021-01-20 20:22:36 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="alxdembo-vtx",
+    version="0.0.1",
+    author="Alex Dembo",
+    author_email="a@dembo.lv",
+    description="Extracts cargo movement durations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alxdembo/vtx",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pandas']
+)
