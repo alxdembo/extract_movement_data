992be3c7c7f62da7f32c0d01e2ff7d709b5b531e
alxdembo <alex.dembo@sainsburys.co.uk>
2021-01-21 00:29:44 +0000

    add comments

--- extract.py
+++ extract.py
@@ -2,20 +2,47 @@ import pandas as pd
 
 
 class CargoData:
+    """
+    Class to perform various calculations over Cargo Data
+    """
     def __init__(self, source_file):
         self.source_file = source_file
-        self.source_df = pd.read_parquet(source_file, engine='pyarrow', columns=[
+
+        # Rows containing NA values are dropped due to unsuitability for the calculations required
+        self.source_df = pd.read_parquet(source_file, engine='pyarrow', use_nullable_dtypes=True, columns=[
             "start_timestamp",
             "end_timestamp",
             "vessel_class",
             "id"
-        ])
+        ]).dropna()
 
     def get_duration(self):
-        return self.source_df.end_timestamp - self.source_df.start_timestamp
+        """
+        Returns movement duration time
+        :return: Pandas series of movement duration time deltas
+        """
+        return self.source_df.end_timestamp.astype("datetime64") - self.source_df.start_timestamp.astype("datetime64")
 
-    def print_movements(source_file):
-        raise NotImplementedError
+    def get_duration_per_class(self):
+        """
+        Returns movement duration time per class
+        :return: Pandas series of movement duration time deltas with class as an index
+        """
+        duration_per_class_df = self.source_df[["id", "vessel_class"]].copy()
+
+        duration_per_class_df["duration"] = self.get_duration()
+
+        return duration_per_class_df.groupby("vessel_class")["duration"].sum()
+
+    def print_movements(self):
+        """
+        Prints movement durations and movement durations per class
+        """
+        duration_df = self.source_df
+        duration_df["duration"] = self.get_duration()
+
+        print("\nMovement duration: \n", duration_df.to_string())
+        print("\nMovement duration per class: \n", self.get_duration_per_class().to_string())
 
 
 if __name__ == '__main__':
@@ -24,5 +51,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("source", help="A source file to extract movements from.")
     args = parser.parse_args()
-
-    print_movements(args.source)
+    #
+    cargo_data = CargoData(args.source)
+    cargo_data.print_movements()
--- test_extract.py
+++ test_extract.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 def cargo_movements(tmpdir):
     df = pd.DataFrame({
         "start_timestamp": [datetime(2000, 1, 1, 1, 1, 1), datetime(2000, 1, 1, 1, 1, 1), datetime(2000, 1, 1, 1, 1, 1)],
-        "end_timestamp": [datetime(2000, 1, 1, 1, 1, 2), datetime(2000, 1, 1, 1, 1, 3), datetime(2000, 1, 1, 1, 1, 4)],
+        "end_timestamp": [datetime(2000, 1, 1, 1, 1, 2), datetime(2000, 1, 1, 1, 1, 3), datetime(2000, 1, 1, 1, 1, 5)],
         "vessel_class": ["test_class_1", "test_class_1", "test_class_2"],
         "id": ["test_id_1", "test_id_2", "test_id_3"]
     })
@@ -18,12 +18,23 @@ def cargo_movements(tmpdir):
     return filename
 
 
-def test_print_movements(cargo_movements):
-    cc = CargoData(cargo_movements)
-    actual = cc.get_duration().to_dict()
+def test_get_duration(cargo_movements):
+    cargo_data = CargoData(cargo_movements)
+    actual = cargo_data.get_duration().to_dict()
     expected = {
         0: timedelta(0,1),
         1: timedelta(0,2),
-        2: timedelta(0,3)
+        2: timedelta(0,4)
     }
+
+    assert actual == expected
+
+def test_get_duration_per_class(cargo_movements):
+    cargo_data = CargoData(cargo_movements)
+    actual = cargo_data.get_duration_per_class().to_dict()
+    expected = {
+        "test_class_1": timedelta(0,3),
+        "test_class_2": timedelta(0,4)
+    }
+
     assert actual == expected
